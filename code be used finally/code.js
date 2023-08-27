ad62adbe1fddd3ef24bd1eaf6e793a3e02de9807
CHOU, TZU-CHIEH <83256594+pinguinjay@users.noreply.github.com>
2023-08-27 08:20:12 +0800

    Update code.js

--- code be used finally/code.js
+++ code be used finally/code.js
@@ -13,7 +13,7 @@ function doGet(){
 //抓整列(row)資料
 function getData(){
      //var activeSheet = SpreadsheetApp.getActiveSpreadsheet(); //抓取目前的試算表
-     var activeSheet = SpreadsheetApp.openById("SpreadSheetID"); //抓取目前的試算表
+    var activeSheet = SpreadsheetApp.openById("SpreadSheetID"); //抓取目前的試算表
     var sheetNo = activeSheet.getSheetByName("sheet2"); //改成要抓的內容的試算表分頁名稱
     var data = sheetNo.getDataRange().getValues(); //抓資料範圍的值   
     return data.slice(1);//第一列是標題，所以從第二列開始抓
