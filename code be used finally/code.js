2b6a3c1366f7d3bcf33e392ff94be0c39a060a59
CHOU, TZU-CHIEH <83256594+pinguinjay@users.noreply.github.com>
2023-08-27 08:19:51 +0800

    Update code.js

--- code be used finally/code.js
+++ code be used finally/code.js
@@ -4,7 +4,8 @@
 function doGet(){
     var html = HtmlService.createTemplateFromFile("DataTable"); //改成要顯示的HTML檔案名稱
     var check = html.evaluate(); //evaluate()會回傳一個HtmlOutput物件
-    //我不知道，我看github上大家都這樣弄，但是我不知道為什麼要這樣寫
+    //X-Frame-Options： X-Frame-Options 是一個 HTTP 響應標頭，它有三個可能的值：DENY、SAMEORIGIN 和 ALLOWALL。這個標頭的主要目的是為了防止「點擊劫持」攻擊，即將此網站嵌入到其他網站的 iframe 中，進行一些惡意操作。
+    //ALLOWALL： HtmlService.XFrameOptionsMode.ALLOWALL 是允許所有的 iframe。這意味著，此網頁可以嵌入到任何網站的 iframe 中，而不受限制
     var display = check.setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL); 
     return display;
 }
